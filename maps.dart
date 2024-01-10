a01d25b80498e16cc65ea5954bc3286e498ce848
marky376 <mmutuamark@gmail.com>
2024-01-10 17:29:57 +0300

    A trial on the dart programming language

--- maps.dart
+++ maps.dart
@@ -3,4 +3,7 @@ void main()
 	List myList = [1, 2, 3];
 	Map myMap = {"Mark": "Nairobi", "Joe" : "Canada", "Kimmy" : "United Kingdom"};
 	print(myMap);
+	print(myMap.key[0]);
+	print(myMap.value[0]);
+
 }
