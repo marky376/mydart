15ed88d9ab4146518376ad2b014e526f680f38ad
marky376 <mmutuamark@gmail.com>
2024-01-10 17:15:29 +0300

    A trial on the dart programming language

--- list.dart
+++ list.dart
@@ -3,4 +3,13 @@ void main()
 	List book = ["English", "Swahili", "Science"];
 	book.add("Geography");
 	print(book);
+
+	book.addAll(["Biology", "Chemistry", "Physics","Computer Studies"]);
+	print(book);
+
+	book.insert(0, "Agriculture");
+	print(book);
+	book.insert(1, "Mathematics");
+	print(book);
+
 }
