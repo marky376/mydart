21a16e28e2e98e50abd710c064fbdc664b82dd6f
marky376 <mmutuamark@gmail.com>
2024-01-11 10:14:11 +0300

    A trial on the dart programming language

--- class2.dart
+++ class2.dart
@@ -1,4 +1,54 @@
 void main()
 {
-	//A class without initialization
+        // Classes init fahm
+
+        Person p1 = Person("John", "Male", 44);
+        p1.showData();
+        print(p1.name);
+        // function calls the method in the class (Person)
+
+        // Create a second person
+        Person p2 = Person("Mark Munyao Mutua", "Male", 19);
+        p2.showData();
+        print(p2.sex);
+
+        // Create a third person
+        Person p3 = Person("Dev. Alex Mwaniki", "Male", 19);
+        p3.showData();
+        print(p3.age);
+
+        // Create a fourth person
+        Person p4 = Person("Dev. Mercy Njoroge", "Female", 22);
+        p4.showData();
+        print(p4.name);
 }
+class Person
+{
+        // Blueprint
+        // the (?) is for nullability
+        String? name, sex;
+        int? age;
+
+        // Constructors
+        Person(String name, sex, int age)
+        {
+                this.name = name;
+                this.sex = sex;
+                this.age = age;
+        }
+        // Method
+        // Named methods when in a class and when not in a class
+        // They are called functions
+
+        // method to be called by the main function of the program
+        void showData()
+        {
+                print('Name = $name');
+                print('Sex = $sex');
+                print('Age = $age');
+
+        }
+        // The end
+
+}
+
