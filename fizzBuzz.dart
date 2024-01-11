0303cc90be4719dd126b567b23da89f395d605ec
marky376 <mmutuamark@gmail.com>
2024-01-11 09:13:09 +0300

    A trial on the dart programming language

--- fizzBuzz.dart
+++ fizzBuzz.dart
@@ -13,11 +13,12 @@ void main()
 		{
 			print("$num, FizzBuzz");	
 		}
-		// Print out num if num % 3 == 0
+		// Print out num if num % 3 == 0 (
 		else if (num % 3 == 0)
 		{
 			print("$num, Fizz");
 		}
+		// Print out num if num % 5 == 0 (Fizz)
 		else if (num % 5 == 0)
 		{
 			print("$num, Fizz");
