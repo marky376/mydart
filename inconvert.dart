d77874596b34edd9a12893036914e77e39e96bc7
marky376 <mmutuamark@gmail.com>
2024-01-11 08:48:14 +0300

    A trial on the dart programming language

---
+++ inconvert.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main()
+{
+	// User input Type conversion
+	print("Enter a number please: ");
+
+	// Get user input
+	var nummy = stdin.readLineSync();
+
+	// Terminate the nulling factor
+	var nummy2 = int.parse(nummy ?? '0') + 10;
+
+	// Printing out the information
+	print("$nu
+}
