dedff1406a7a485ec4ef64cd1ef60b56da934119
marky376 <mmutuamark@gmail.com>
2024-01-11 10:17:09 +0300

    A trial on the dart programming language

--- class2.dart
+++ class2.dart
@@ -3,24 +3,24 @@ void main()
         // Classes init fahm
 
         Person p1 = Person("John", "Male", 44);
-        p1.showData();
-        print(p1.name);
+        //p1.showData();
+        //print(p1.name);
         // function calls the method in the class (Person)
 
         // Create a second person
-        Person p2 = Person("Mark Munyao Mutua", "Male", 19);
-        p2.showData();
-        print(p2.sex);
+        //Person p2 = Person("Mark Munyao Mutua", "Male", 19);
+        //p2.showData();
+        //print(p2.sex);
 
         // Create a third person
-        Person p3 = Person("Dev. Alex Mwaniki", "Male", 19);
-        p3.showData();
-        print(p3.age);
+        //Person p3 = Person("Dev. Alex Mwaniki", "Male", 19);
+        //p3.showData();
+        //print(p3.age);
 
         // Create a fourth person
-        Person p4 = Person("Dev. Mercy Njoroge", "Female", 22);
-        p4.showData();
-        print(p4.name);
+        //Person p4 = Person("Dev. Mercy Njoroge", "Female", 22);
+        //p4.showData();
+        //print(p4.name);
 }
 class Person
 {
